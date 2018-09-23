5bc0991b3e7062f6713d344ef158aa56ea3efa35
1692378903 <1692378903@qq.com>
2018-09-23 11:27:05 +0800

    888

--- app/src/main/java/com/pickerview/pickerviewdemo/MainActivity.java
+++ app/src/main/java/com/pickerview/pickerviewdemo/MainActivity.java
@@ -19,6 +19,7 @@ import db.ProvinceBean;
 public class MainActivity extends AppCompatActivity {
 
 
+
     private TextView tvTitle;
     private OptionsPickerView pvOptions;//地址选择器
     private ArrayList<ProvinceBean> options1Items = new ArrayList<>();//省
